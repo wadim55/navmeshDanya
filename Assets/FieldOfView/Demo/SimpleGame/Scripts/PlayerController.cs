69d910c8d490f898cabd8a215e73b4d1ed93440d
wadim55 <korkin_wadim@mail.ru>
2022-11-05 18:55:58 +0600

    1

---
+++ Assets/FieldOfView/Demo/SimpleGame/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour 
+{
+    public GameObject NavigationSurface;
+    
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start() {
+        this._navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            MovePlayerTo(Input.mousePosition);
+        }
+    }
+
+    private void MovePlayerTo(Vector3 position)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.collider.gameObject == this.NavigationSurface) {
+                this._navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
